516d0271f17dae6692a1face4780bf6f40a38773
ILYA EVDOKIMOV <miniservis@inbox.ru>
2022-08-30 16:32:52 +0300

    BUK_SVOY_30

--- HW/Test3_7.java
+++ HW/Test3_7.java
@@ -1,16 +1,16 @@
 public class Test3_7 {
     public static void main(String [] args) {
+        System.out.printf("%7s%7s%n%n", "Dec", "Char");
         for (char i = 0; i <= 47; i++) {
-            if (((int) i) % 2 != 0) {
-                System.out.printf("%6s%6s%n", ((int) i), i);
-                //System.out.println();
+            if ( i % 2 != 0) {
+                System.out.printf("%6d%6c%n", ((int) i), i);
             }
         }
         System.out.println();
+        System.out.printf("%7s%7s%n%n", "Dec", "Char");
         for (char i = 97; i <= 122; i++) {
-            if (((int) i) % 2 == 0) {
-                System.out.printf("%6s%6s", ((int) i), i);
-                System.out.println();
+            if ( i % 2 == 0) {
+                System.out.printf("%6d%6c%n", ((int)i), i);
             }
         }
     }
--- HW/Test_3_9.java
+++ HW/Test_3_9.java
@@ -1,28 +1,43 @@
 public class Test_3_9 {
     public static void main(String[] agrs){
-        int num = 657576;
+        int num = 214421;
         int rem = 0;
-        int rev = 0;
         int sum = 0;
         int count = 0;
-        int rev1 = 0;
         int rem1 = 0;
         int sum1 = 0;
         int count1 = 0;
-        while (count <= 2) {
+/*        while (count <= 5) {
+            if (count <= 2) {
             rem = num % 10;
-            rev = rev * 10 + rem;
-            sum = sum + rem;
-            num = num / 10;
+            sum += rem;
+            num /= 10;
+            } else {
+                rem1 = num % 10;
+                sum1 = sum1 + rem1;
+                num /= 10;
+            }
+            if (count == 6) {
+                break;
+            }
             count++;
         }
-        while (count1 <= 2) {
-            rem1 = num % 10;
-            rev1 = rev1 * 10 + rem1;
-            sum1 = sum1 + rem1;
-            num = num / 10;
-            count1++;
+        //while (count1 <= 2) {
+            
+            //count1++;
+        //}*/
+
+        for ( int i = 0; i <= 5; i++) {
+            if (i <= 2) {
+                rem = num % 10;
+                sum += rem;
+            } else {
+                rem = num % 10;
+                sum1 += rem;
+            }
+            num /= 10;
         }
+
         if (sum == sum1) {
             System.out.println("Сумма цифр sum = sum1: " + sum + " = " + sum1 + "\nЧисло является счастливым");
         } else {
