b1759f71e36daad6678ae9181d574057fca1c776
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-09-11 23:44:39 +0300

    HW_SVOY_6_10

--- HW/Test_3_10.java
+++ HW/Test_3_10.java
@@ -5,6 +5,17 @@ public class Test_3_10 {
             //System.out.printf("%3d", i);
         //}
 
+        for (int i = 2; i <= 9; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+        for (int i = 2; i < 10; i++) {
+            for(int j = 1; j < 10; j++) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
+
 
         /*int count = 0;
         System.out.println();
@@ -30,27 +41,43 @@ public class Test_3_10 {
         //int i = 0;
         //int j = 0;
 
-        for (int i = 1; i < 10; i++) {
+        /*for (int i = 1; i < 10; i++) {
             if(i == 2) {
-                System.out.println(" ______________________________");
+                System.out.println(" ___________________________");
             }
             for (int j = 1; j < 10; j++)
                 if(j == 1) {
-                    System.out.printf("%4d %1s", i * j, (char) 124);
+                    System.out.printf("%1d%1s", i * j, (char) 124);
                 } else {
                     System.out.printf("%3d", i * j);
                 }
             System.out.println();
-        }
+        }*/
 
         System.out.print("\tТАБЛИЦА ПИФАГОРА 111 " + "\n   ");
         for (int i = 2; i <= 9; i++) {
-            System.out.printf("%3d", i);
+            if(i == 2) {
+                System.out.printf("%1s%3d", (char) 124, i);
+
+            } else {
+                System.out.printf("%3d", i);
+
+            }
         }
+           //System.out.println(" -------------");
+
         System.out.println();
         for (int i = 2; i < 10; i++) {
+            if(i == 2) {
+                System.out.println("  --------------------------");
+                //System.out.printf("%1s", (char) 95);
+            }
             for(int j = 1; j < 10; j++) {
-                System.out.printf("%3d %1s", i * j, (char) 124);
+                if(j == 1) {
+                System.out.printf("%3d%1s", i * j, (char) 124);
+            } else {
+                System.out.printf("%3d", i * j);
+            }
             }
             System.out.println();
         }
