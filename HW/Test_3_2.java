fae3d2a592707cf991d19f872980a652a2a0184d
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-09-14 22:23:41 +0300

    HW_SVOY_6_11

--- HW/Test_3_2.java
+++ HW/Test_3_2.java
@@ -1,7 +1,7 @@
 public class Test_3_2 {
 
     public static void main(String[] agrs) {
-        System.out.println("\t" + "ЗАДАНИЕ 2 \nВывод чисел в интервале (min и max) в порядке убывания\n");
+        /*System.out.println("\t" + "ЗАДАНИЕ 2 \nВывод чисел в интервале (min и max) в порядке убывания\n");
         int num1 = 10;
         int num2 = 5;
         int num3 = -1;
@@ -22,6 +22,30 @@ public class Test_3_2 {
             min = num3;
         }
         for (int i = max - 1; i >= min + 1; i--) {
+            System.out.print(" " + i);
+        }*/
+
+        System.out.println("\t" + "ЗАДАНИЕ 2 \nВывод чисел в интервале (min и max) в порядке убывания\n");
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = -1;
+        int max = 0;
+        int min = 0;
+        if (num2 < num1 && num1 > num3) {
+            max = num1;
+        } else if (num3 < num2 && num2 > num1) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+        if (num2 > num1 && num1 < num3) {
+            min = num1;
+        } else if (num1 > num2 && num2 < num3) {
+            min = num2;
+        } else {
+            min = num3;
+        }
+        for (int i = max - 1; i > min; i--) {
             System.out.print(" " + i);
         }
     }
