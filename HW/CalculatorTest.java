ff36c9dd04db96c71f93a374949ea1e8231f3710
ILYA EVDOKIMOV <miniservis@inbox.ru>
2022-09-18 15:45:35 +0300

    HW_BUK_34

--- HW/CalculatorTest.java
+++ HW/CalculatorTest.java
@@ -2,8 +2,8 @@ public class CalculatorTest {
     public static void main(String [] args){
         int num1 = 10;
         int num2 = 5;
-        int result = 0;
-        char sign = '%';
+        int result = 1;
+        char sign = '+';
         if (sign == '+') {
             result = num1 + num2;
         } else if (sign == '-') {
@@ -13,7 +13,7 @@ public class CalculatorTest {
         } else if (sign == '/') {
             result = num1 / num2;
         } else if (sign == '^') {
-            result = 1;
+            //result = 1;
             for (int i = 1; i <= num2; i++) {
                 result *= num1;
             }
