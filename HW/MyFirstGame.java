bbe6812c042625e2ff3c7a47beebf7e862527691
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-09-18 23:15:50 +0300

    HW_SVOY_6_12

--- HW/MyFirstGame.java
+++ HW/MyFirstGame.java
@@ -1,13 +1,30 @@
 public class MyFirstGame {
     public static void main(String [] args){
         int computerNum = 50;
-        int playerNum = 20;
+        int playerNum = 50;
         boolean neugadal = true;
         
         while (neugadal) {
+            int count = 0;
+            if (count % 2 == 0) {
+                for (int i = 1; i <= 100; i += 3) {
+
+            }
+                    playerNum = i
+                }
+            
+             
+            if (computerNum < playerNum) {
+                System.out.println("Вы не угадали, загаданное число меньше! Попытайтесь еще раз!");
+            }
+            if (computerNum > playerNum) {
+                System.out.println("Вы не угадали, загаданное число больше! Попытайтесь еще раз!");
+            }
             if (playerNum == computerNum) {
+                System.out.println("Вы угадали, поздравляю!");
                 neugadal = false;
             }
+            count++;
         }
 
     }
