6b70ba3fdef0518a45a425cf33b25d0570e9cb17
ILYA EVDOKIMOV <miniservis@inbox.ru>
2022-09-18 22:54:25 +0300

    HW_BUK_35

---
+++ HW/MyFirstGame.java
@@ -0,0 +1,14 @@
+public class MyFirstGame {
+    public static void main(String [] args){
+        int computerNum = 50;
+        int playerNum = 20;
+        boolean neugadal = true;
+        
+        while (neugadal) {
+            if (playerNum == computerNum) {
+                neugadal = false;
+            }
+        }
+
+    }
+}
