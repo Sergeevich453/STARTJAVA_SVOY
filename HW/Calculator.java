c503bd68cb69040fb5e99f80a922dad4b7cbb6c9
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-10-24 23:56:37 +0300

    HW_SVOY_6_22

--- HW/Calculator.java
+++ HW/Calculator.java
@@ -1,25 +1,69 @@
+
+import java.util.Scanner;
 public class Calculator {
-    
-    public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 5;
         int result = 1;
-        char sign = '+';
-        if (sign == '+') {
-            result = num1 + num2;
-        } else if (sign == '-') {
-            result = num1 - num2;
-        } else if (sign == '*') {
-            result = num1 * num2;
-        } else if (sign == '/') {
-            result = num1 / num2;
-        } else if (sign == '^') {
-            for (int i = 1; i <= num2; i++) {
-                result *= num1;
+        Scanner s = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        //System.out.println("Введите первую цифру: ");
+        int num1 = s.nextInt();
+        //System.out.println("Введите арифметический знак: ");
+        char sign = scan.nextLine().charAt(0);
+        //System.out.println("Введите вторую цифру: ");
+        int num2 = s.nextInt();
+        public char vichisl(char sign) {
+            this.sign = sign;
+            switch(sign) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case '^':
+                    for(int i = 1; i <= num2; i++) {
+                        result *= num1;
+                    }
+                    break;
+                case '%':
+                    result = num1 % num2;
+                    break;
+                default: 
+                    System.out.println("Вы ввели некорректное значение повторите попытку.");
+                    break;
             }
-        } else if (sign == '%') {
-            result = num1 % num2;
+            return result;
         }
-        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
-    }
-}
+
+        //System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+        //System.out.println(num1);
+
+    
+}    
+    // public static void main(String[] args) {
+    //     int num1 = 10;
+    //     int num2 = 5;
+    //     int result = 1;
+    //     char sign = '+';
+    //     if (sign == '+') {
+    //         result = num1 + num2;
+    //     } else if (sign == '-') {
+    //         result = num1 - num2;
+    //     } else if (sign == '*') {
+    //         result = num1 * num2;
+    //     } else if (sign == '/') {
+    //         result = num1 / num2;
+    //     } else if (sign == '^') {
+    //         for (int i = 1; i <= num2; i++) {
+    //             result *= num1;
+    //         }
+    //     } else if (sign == '%') {
+    //         result = num1 % num2;
+    //     }
+    //     System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    // }
--- HW/CalculatorTest.java
+++ HW/CalculatorTest.java
@@ -1,25 +1,18 @@
+import java.util.Scanner;
+
 public class CalculatorTest {
+
     public static void main(String [] args){
-        int num1 = 10;
-        int num2 = 5;
-        int result = 1;
-        char sign = '+';
-        if (sign == '+') {
-            result = num1 + num2;
-        } else if (sign == '-') {
-            result = num1 - num2;
-        } else if (sign == '*') {
-            result = num1 * num2;
-        } else if (sign == '/') {
-            result = num1 / num2;
-        } else if (sign == '^') {
-            //result = 1;
-            for (int i = 1; i <= num2; i++) {
-                result *= num1;
-            }
-        } else if (sign == '%') {
-            result = num1 % num2;
-        }
-        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
-    }
+        Calculator b = new Calculator();
+        // int result = 1;
+        // Scanner s = new Scanner(System.in);
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Введите первую цифру: ");
+        // int num1 = s.nextInt();
+        // System.out.println("Введите арифметический знак: ");
+        // char sign = scan.nextLine().charAt(0);
+        // System.out.println("Введите вторую цифру: ");
+        // int num2 = s.nextInt();
+        b.
+
 }
