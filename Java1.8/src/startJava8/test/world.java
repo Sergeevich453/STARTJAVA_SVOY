9e6d672666da346e80ea37fd11ff3be2f06014cc
Ilya Test <miniservis@inbox.ru>
2024-02-07 23:59:00 +0300

    Test komp Java8

---
+++ Java1.8/src/startJava8/test/world.java
@@ -0,0 +1,11 @@
+package startJava8.test;
+
+public class world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
