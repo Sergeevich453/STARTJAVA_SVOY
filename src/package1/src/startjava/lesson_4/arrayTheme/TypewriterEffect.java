07e78a7581cf46f7a014f5f0eb96c9a36aff1c78
Ilya Buk <miniservis@inbox.ru>
2024-03-05 16:30:59 +0300

    Buk eclipse

--- src/package1/src/startjava/lesson_4/arrayTheme/TypewriterEffect.java
+++ src/package1/src/startjava/lesson_4/arrayTheme/TypewriterEffect.java
@@ -1,6 +1,7 @@
 package package1.src.startjava.lesson_4.arrayTheme;
 
 public class TypewriterEffect {
+	// 1
     public static void main(String[] args) {
         String text = "Java — это C++, из которого убрали все пистолеты, ножи и дубинки.\n— James Gosling";
         printWithTypewriterEffect(text);
@@ -8,6 +9,26 @@ public class TypewriterEffect {
         // Проверка на другом тексте
         String anotherText = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n— Robert Martin";
         printWithTypewriterEffect(anotherText);
+    }
+    
+    private static String findShortestWord(String[] words) {
+        String shortestWord = words[0];
+        for (String word : words) {
+            if (word.length() < shortestWord.length()) {
+                shortestWord = word;
+            }
+        }
+        return shortestWord;
+    }
+
+    private static String findLongestWord(String[] words) {
+        String longestWord = words[0];
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        return longestWord;
     }
 
     private static void printWithTypewriterEffect(String text) {
@@ -29,23 +50,5 @@ public class TypewriterEffect {
         System.out.println("Самое длинное слово: " + longestWord.toUpperCase());
     }
 
-    private static String findShortestWord(String[] words) {
-        String shortestWord = words[0];
-        for (String word : words) {
-            if (word.length() < shortestWord.length()) {
-                shortestWord = word;
-            }
-        }
-        return shortestWord;
-    }
-
-    private static String findLongestWord(String[] words) {
-        String longestWord = words[0];
-        for (String word : words) {
-            if (word.length() > longestWord.length()) {
-                longestWord = word;
-            }
-        }
-        return longestWord;
-    }
+    
 }
