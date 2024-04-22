8912d56be8f6004d3c5c98a0efaade498f6315b3
Ilya Test <miniservis@inbox.ru>
2024-04-22 23:37:45 +0300

    Home

--- src/codeSignale/intro/centuryFromYear.java
+++ src/codeSignale/intro/centuryFromYear.java
@@ -11,10 +11,24 @@ public class centuryFromYear {
 		return year;
 	}
 	
+	private static boolean solution(String inputString) {
+		int i = 0;
+		int j = inputString.length() - 1;
+		while (i < j) {
+			if (inputString.charAt(i) != inputString.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+	
 
 	public static void main(String[] args) {
 		int year = 1700;
 		System.out.println(fromYear(year));
+		System.out.println(solution("abca"));
 
 	}
 
