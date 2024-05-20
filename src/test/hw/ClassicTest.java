8165e029c49ab56a41936d4a8a4520f6a741c218
Ilya Buk <miniservis@inbox.ru>
2024-05-20 16:56:42 +0300

    Buk

---
+++ src/test/hw/ClassicTest.java
@@ -0,0 +1,23 @@
+package test.hw;
+
+import java.util.stream.IntStream;
+
+public class ClassicTest {
+	private int last = 0;
+	private int next = 1;
+	
+	public intStreat stream() {
+		return IntStream.generate(() -> {
+			int oldLast = last;
+			last = next;
+			next = oldLast + next;
+			return oldLast;
+		});
+	}	
+
+	public static void main(String[] args) {
+		ClassicTest fab5 = new ClassicTest();
+		fab5.stream().limit(41).forEachOrdered(System.out::println);
+	}
+
+}
